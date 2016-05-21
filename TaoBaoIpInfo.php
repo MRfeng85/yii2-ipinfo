9c4caaa6bff554ee9e63d98b45b5d85e2eb857c3
Mrfeng <83398365@qq.com>
2016-05-21 15:06:36 +0800

    wrote a Tao Bao Ip Info file

---
+++ TaoBaoIpInfo.php
@@ -0,0 +1,28 @@
+<?php
+	namespace mrfeng\ipinfo;
+	/**
+	* Tao Bao Ip Info
+	* @author Mrfeng<https://github.com/MRfeng85>
+	*/
+	class TaoBaoIpinfo
+	{
+		public $url = 'http://ip.taobao.com/service/getIpInfo.php?ip=';
+
+		function __construct(argument)
+		{
+			# code...
+		}
+		public function getinfo($ip){
+			$this->url .= $ip;
+			$ch = curl_init($this->url);
+			$ch = curl_setopt($ch,CURLOPT_ENCODING,'utf-8');
+			$ch = curl_setopt($ch,CURLOPT_TIMEOUT,10);
+			$ch = curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+			$resurl = curl_exec($ch);
+			$resurl = json_decode($resurl);
+			curl_close($ch);
+			if($resurl->code) return "";
+			return  $resurl->code?"":$resurl->data;
+		}
+	}
+?>
